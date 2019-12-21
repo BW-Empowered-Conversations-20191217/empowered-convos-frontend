d778c880611926cb044f7636bda165cf953aeba8
lee-wood <leemaewood@gmail.com>
2019-12-20 22:33:28 -0500

    base user page/form set

---
+++ empowered-convos/UserPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserPage = (props) => {
+  return(
+    <div>
+      <h1>Welcome {props.user.name}</h1>
+      {/*Use Our Tool/form goes here*/}
+    </div>
+  )
+}
