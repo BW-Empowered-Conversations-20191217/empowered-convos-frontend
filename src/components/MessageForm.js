48cfdd9632f4e20c640dd8aeb2a1a312bfd772b9
lee-wood <leemaewood@gmail.com>
2020-01-06 22:17:46 -0500

    pulled from master

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const MessageForm = () => {
   //TODO figure out how to integrate Twilio into sending a message. For test purposed I might have to set up a Twilio message receiving account.
+
   return (
     <div>
       <h1>Time to Reach Out</h1>
