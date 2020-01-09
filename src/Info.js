49021fd0904f2df5ff32c1b2b59089e44ba26ad4
Vincent Adeniji <adenijitosin112@gmail.com>
2020-01-09 00:22:42 -0500

    Changed Favicon

--- src/Info.js
+++ src/Info.js
@@ -10,7 +10,7 @@ const Info = props => {
             <Grid className="image">
                 <Cell col={6}> 
                 <h1> {props.firstName} {props.lastName} VINCENT ADENIJI </h1>
-                <img className="img" src={props.im2g} src="1.jfif"  alt="front" />
+                <img className="img" src={props.im2g}  alt="img" />
                 <p> {props.about} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </Cell>
 
@@ -22,7 +22,7 @@ const Info = props => {
                     <div className="ListItems">
                     <List>
                         <ListItem className="detailContainer">
-                            <ListItemContent icon="person" className="user" >{props.code} hfdfjsdo</ListItemContent>
+                            <ListItemContent icon="person" className="user" >{props.code}</ListItemContent>
                         </ListItem>
                         <ListItem className="detailContainer">
                             <ListItemContent icon="my_location" className="user">{props.code}</ListItemContent>
