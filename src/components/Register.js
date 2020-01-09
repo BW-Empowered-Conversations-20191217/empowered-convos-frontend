4e97fb6715ca2a16db8ec84689d1dcbc05db749e
Leisha <banksleisha@yahoo.com>
2020-01-09 01:15:38 -0500

    registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,29 +2,84 @@ import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
+import styled from 'styled-components';
 
+const RegFormStyle = {
+    background: "white",
+    margin:" 0 auto",
+    boxShadow: "0px 2px 1px rgba(0, 0, 0, 0, 1)",
+    left: "489px",
+    right: "302px",
+    borderRadius: "29.5px",
+    border: "1px solid #5440c0",
+};
 
+const RegistrationBackground = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+width: 100%;
+height: 700px;
+background: #5440c0;
+`;
 
+const RegistrationForm = styled.div`
+    background: white;
+    margin: 0 auto;
+    box-shadow: 0px 2px 1px rgba(0, 0, 0, 0, 1);
+    width: 462px;
+    height: 419px;
+    left: 489px;
+    right: 302px;
+    border-radius: 29.5px;
+    display: flex; 
+    flex-direction: column;
+    align-items: center;
+`;
 
+const Button = styled.button`
+    background: #5440c0;
+    width: 184px;
+    height: 51px;
+    left: 628px;
+    right: 579px;
+    border-radius: 29.5px;
+    display: flex;
+    flex-direction: column;
+    font-size: 20px;
+    color: white;
+    font-weight: bold;
+`;
+
+const Title = styled.h2`
+font-size: 30px;
+`
 
 function Register({ values, errors, touched }) {
 console.log(touched);
 return(
- 
+    <RegistrationBackground>
+        <RegistrationForm>
+            <Title>Register</Title>
        <Form>
         <div>
 
         <div>
-            {touched.name && errors.name && <p>{errors.name}</p>}
-            <Field type="name" name="name" placeholder="Username" />
+            {touched.username && errors.username && <p>{errors.username}</p>}
+            <Field type="username" name="username" placeholder="Username" style={RegFormStyle} />
+        </div>
+        <div>
+            {touched.full_name && errors.full_name && <p>{errors.full_name}</p>}
+            <Field type="name" name="name" placeholder="Name" style={RegFormStyle} />
         </div>
          <div>
             {touched.email && errors.email && <p>{errors.email}</p>}
-            <Field type="email" name="email" placeholder="Username" /> 
+            <Field type="email" name="email" placeholder="Email" style={RegFormStyle} /> 
          </div>
          <div>
             {touched.password && errors.password && <p>{errors.password}</p>}
-            <Field type="password" name="password" placeholder="Password" />
+            <Field type="password" name="password" placeholder="Password" style={RegFormStyle} />
          </div>
          <div>
              {touched.full_name && errors.full_name && <p>{errors.full_name}</p>}
@@ -35,16 +90,18 @@ return(
             <Field type="checkbox" name="tos" checked={values.tos} />
             Agree to Terms of Service
          </label>
-            <button type='submit'>Register</button>
+            <Button type='submit'>Register</Button>
         
             </Form> 
-     
+        </RegistrationForm>    
+    </RegistrationBackground>
  );
 }
 
 const FormikRegistrationForm = withFormik({
-mapPropsToValues({ name, email, password, tos }) {
+mapPropsToValues({ username, name, email, password, tos }) {
     return {
+        username: username || "",
         name: name || "",
         email: email || "",
         password: password || "",
@@ -53,9 +110,11 @@ mapPropsToValues({ name, email, password, tos }) {
 },
 
 validationSchema: Yup.object().shape({
-    Username: Yup.string()
+    username: Yup.string()
         .min(8, "Username must be 8 characters or longer")
-        .required("Name is Required"),
+        .required("Username is Required"),
+    name: Yup.string()
+        .required("Name is required"),
     email: Yup.string()
         .email("Email not valid")
         .required("Email is required"),
