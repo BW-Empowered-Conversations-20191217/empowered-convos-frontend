6c01203c4e47605324cedb7b564fdeed0efb40aa
lee-wood <leemaewood@gmail.com>
2020-01-10 20:13:46 -0500

    combined new nav with old nav, temp disabled the page not found alert

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navigation from "./navigation"
 import Contactdata from "./contactdata"
-import Contact from './components/contact';
+//import Contact from './components/contact';
+import Profile from './components/Profile';
 //import Navigation from './components/header';
 import MessageForm from './components/MessageForm';
 import PrivateRoute from './utils/PrivateRoute';
@@ -20,13 +21,13 @@ const App = () => {
       <Switch>
         <Route exact path="/profile" component={Contactdata} />
         {/* <Route path="/contactus" component={ContactUs} /> */}
-        <Route render={() => <h1 className="error">Sorry!!! Page Not Found </h1>} />
+        {/* <Route render={() => <h1 className="error">Sorry!!! Page Not Found </h1>} /> */}
       </Switch>
 
       <Route exact path='/' component={Login} />
       <Route path='/register' component={Register} />
       <PrivateRoute path='/message' component={MessageForm} />
-      <PrivateRoute path='/userpage' component={Contact} />
+      <PrivateRoute path='/userpage' component={Profile} />
     </div>
   ) 
 }
